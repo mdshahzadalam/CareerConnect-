58e8b1afbe6b4b4df580a1a9e1659d6d8adf92b5
mdshahzadalam <mdshahzadalam69@gmail.com>
2025-08-25 14:56:44 +0530

    add preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+
+
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  preview: {
+    allowedHosts: ["careerconnectpro.onrender.com"],
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+  }
 })
